bfc3a2a45e9ec29d4b852f4799ff59f739b7e1c7
kimsolee95 <ahhasolee@naver.com>
2022-12-27 15:06:17 +0900

    프로젝트 build

---
+++ src/main/java/com/shoptest/catmart/CatmartApplication.java
@@ -0,0 +1,13 @@
+package com.shoptest.catmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoptest/catmart/CatmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoptest.catmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
