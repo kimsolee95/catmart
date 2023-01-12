47ccf6fa229202d0639de1ca419e6792c5985d0b
kimsolee95 <ahhasolee@naver.com>
2023-01-12 18:39:54 +0900

    전역 핸들러 내 장바구니 에러 잡는 메서드에서 400 코드로 에러 결과 응답값으로 내려주게 한 후, 해당 api 이용하는 화면단 내에서 에러 결과 메시지 alert창으로 보이게끔 코드 추가

--- src/main/java/com/shoptest/catmart/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shoptest/catmart/common/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.shoptest.catmart.common.exception;
 
 import com.shoptest.catmart.common.exception.model.CartErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
@@ -14,6 +16,7 @@ public class GlobalExceptionHandler {
   * 장바구니 API 호출 에러 핸들러
   * */
   @ResponseBody
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(CartException.class)
   public CartErrorResponse handleCartException(CartException e) {
 
