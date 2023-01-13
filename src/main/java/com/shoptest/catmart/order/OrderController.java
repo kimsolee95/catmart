cef7388230fb1ea8339bd02f75dd3f12196a347d
kimsolee95 <ahhasolee@naver.com>
2023-01-14 01:31:34 +0900

    주문결제 페이지 퍼블리싱

---
+++ src/main/java/com/shoptest/catmart/order/OrderController.java
@@ -0,0 +1,27 @@
+package com.shoptest.catmart.order;
+
+import com.shoptest.catmart.cart.service.CartService;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+  private final CartService cartService;
+
+  @GetMapping
+  public String placeAnOrderForm(Model model, Principal principal) {
+
+    String email = principal.getName();
+    model.addAttribute("cartItemList", cartService.selectCartItemDetailList(email));
+
+    return "/order/place_an_order";
+  }
+
+}
