e5e53ab3b8d3a716a2c0fdda7a62c1fa038d6da7
kimsolee95 <ahhasolee@naver.com>
2023-01-20 01:45:07 +0900

    상품등록 부분 API 호출로 변경( @Valid 사용 및 전역익셉션 핸들러에서 유효성검사 실패시 에러 메시지 사용자가 받게끔 수정 )

--- src/main/java/com/shoptest/catmart/admin/controller/ApiManagingShopResourceController.java
+++ src/main/java/com/shoptest/catmart/admin/controller/ApiManagingShopResourceController.java
@@ -4,6 +4,8 @@ package com.shoptest.catmart.admin.controller;
 import com.shoptest.catmart.admin.dto.CategoryDto;
 import com.shoptest.catmart.admin.service.CategoryService;
 import com.shoptest.catmart.common.model.ResponseResult;
+import com.shoptest.catmart.product.dto.ProductItemDto;
+import com.shoptest.catmart.product.service.ProductService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,17 +15,30 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * parameter valid 공통 익셉션 적용 위한 ADMIN 상품 등록 api
+ * */
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class ApiManagingCategoryController {
+public class ApiManagingShopResourceController {
 
   private final CategoryService categoryService;
+  private final ProductService productService;
 
   @PostMapping("/api/admin/newCategory")
   public ResponseEntity<?> addNewCategory(@RequestPart("data") @Valid CategoryDto categoryDto, @RequestPart MultipartFile fileInput) {
 
     categoryService.saveCategory(categoryDto, fileInput);
+
+    ResponseResult responseResult = new ResponseResult(true);
+    return ResponseEntity.ok().body(responseResult);
+  }
+
+  @PostMapping("/api/admin/newProduct")
+  public ResponseEntity<?> addNewProduct(@RequestPart("data") @Valid ProductItemDto productItemDto, @RequestPart MultipartFile fileInput) {
+
+    productService.saveProduct(productItemDto, fileInput);
 
     ResponseResult responseResult = new ResponseResult(true);
     return ResponseEntity.ok().body(responseResult);
--- src/main/java/com/shoptest/catmart/product/dto/ProductItemDto.java
+++ src/main/java/com/shoptest/catmart/product/dto/ProductItemDto.java
@@ -1,6 +1,9 @@
 package com.shoptest.catmart.product.dto;
 
 import com.shoptest.catmart.product.type.ItemStatus;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,21 +18,26 @@ import lombok.ToString;
 public class ProductItemDto {
 
   /* 상품 카테고리 ID */
+  @Positive(message = "유효하지 않은 카테고리값입니다.")
   private Long categoryId;
 
   /* 상품이름 */
+  @NotBlank(message = "상품이름은 상품 등록 시 필수입니다.")
   private String itemName;
 
   /* 상품가격 */
+  @Positive(message = "상품값은 0원 이상일 때만 상품 등록 가능합니다.")
   private int price;
 
   /* 상품 상태 - (판매중, 품절) */
   private ItemStatus itemStatus;
 
   /* 상품내용 */
+  @NotBlank(message = "상품 내용은 상품 등록 시 필수입니다.")
   private String itemSubject;
 
   /* 상품재고 */
+  @Min(value = 5, message = "상품 첫 등록 시, 상품의 재고는 5개 이상이어야 합니다.")
   private int stock;
 
   /* 상품 게시 여부 */
